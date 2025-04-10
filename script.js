afed7610a73de16ddf996be907dbdd9678e5328a
ryder <tootwo2to22@gmail.com>
2025-04-09 18:20:41 -0700

    gamenfihqnsjdvah hdf lvmajfd ,va

---
+++ script.js
@@ -0,0 +1,27 @@
+let playerCount = 3;
+
+let player1 = new Set()
+let player2 = new Set()
+let player3 = new Set()
+
+let players = new Map()
+
+let games = ['stardew valley','binding of issac','inscription','supermario','sonic.exe','not a game']
+
+function addGame(player, game){
+    if(){
+
+    }
+    else{
+        
+    }
+}
+
+function newplayer(){
+
+}
+
+function sumgamescore(player){
+
+}
+
