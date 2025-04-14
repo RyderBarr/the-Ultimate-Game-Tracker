4de1bd1dd6dcf162c3d88322e30a856eeb07478b
ryder <tootwo2to22@gmail.com>
2025-04-13 17:48:21 -0700

    version one done

--- gameTracker.js
+++ gameTracker.js
@@ -12,39 +12,61 @@ let gameScores = [
 
 let games = ['stardew valley','cod', 'minecraft', 'snake', 'metal gear solid', 'binding of issac','inscription','supermario','sonic.exe','there is no game']
 
-// not fully commented 
 // adds scores to each player for each game they play the score is random
     function addScores()
     {
-        for(player of players)
-        {
-            tempList = [player[0]]
-            for (game of player[1])
+
+        // clears the gameScores list
+        delete gameScores
+        gameScores = []
+        
+        // goes through all the players 
+            for(player of players)
             {
-                tempList.push(game[1])
+
+                let tempList = [player[0]]
+                
+                // goes through all the games of the player
+                    for (game of player[1])
+                    {
+                        
+                        tempList.push(game[1])
+                    
+                    }
+                
+                // adds a spot for the avarage
+                    tempList.push(0)
+
+                // adds this list to the gameScores list
+                    gameScores.push(tempList)
+            
             }
-            tempList.push(0)
-            gameScores.push(tempList)
-        }
 
     }
 
-// not fully commented 
 // adds a random game to the set
     function addGameRandomly(player){
         
         // get a random index number
-        let index = Math.floor(Math.random() * games.length)
+            let index = Math.floor(Math.random() * games.length)
 
-        if(!player.has(games[index])){
-            player.add([games[index], Math.floor(Math.random() * 40 + 50)])
-        }
-        else{
-            addGameRandomly(player) 
-        }
+        // makes sure the player doesn't have the game already
+            if(!player.has(games[index]))
+            {
+
+                // adds a random score to the game then adds them to the player
+                    player.add([games[index], Math.floor(Math.random() * 40 + 50)])
+            
+            }
+            else
+            {
+                // calls this functions again
+                addGameRandomly(player) 
+
+            }
+
     }
 
-// not fully commented 
 // use to add a game to the set, with the name of the game
     function addGame(player, game, score)
     {
@@ -59,7 +81,7 @@ let games = ['stardew valley','cod', 'minecraft', 'snake', 'metal gear solid', '
             else
             {
 
-                addGame(player) 
+                return 
 
             }
 
@@ -87,7 +109,6 @@ let games = ['stardew valley','cod', 'minecraft', 'snake', 'metal gear solid', '
 
     }
 
-// not fully commented 
 // use to add a player to the map
     function newPlayer(database = users, player, playerName){
 
@@ -96,7 +117,8 @@ let games = ['stardew valley','cod', 'minecraft', 'snake', 'metal gear solid', '
             {
                 
                 playerCount++
-                database.set(playerName ,player)
+                // adds the player to the users
+                    database.set(playerName ,player)
             
             }
             else
@@ -117,12 +139,10 @@ let games = ['stardew valley','cod', 'minecraft', 'snake', 'metal gear solid', '
 // adds three RANDOM games to player3
     addGameRandomly(player3);addGameRandomly(player3);addGameRandomly(player3)
 
-
-
 // adds all the players to the map
     newPlayer(players, player1,'master chief');newPlayer(players, player2, 'markaplier');newPlayer(players, player3, 'ryder barr')
 
-
+// makes a list with all the scores
     addScores()
 
 // skips the first and last of each row
@@ -136,16 +156,61 @@ let games = ['stardew valley','cod', 'minecraft', 'snake', 'metal gear solid', '
             {
 
                 avergeScore += gameScores[i][j] 
-                console.log(gameScores[i][j])
             }
 
         // addes the avarege score to the end of the each row
-            gameScores[i][gameScores[i].length-1] = Math.floor( avergeScore/gameScores[i].length-2)
+            gameScores[i][gameScores[i].length-1] = Math.round( avergeScore/(gameScores[i].length-2))
             
     }
 
-// test area
+console.log('player Game Summary:')
 
-    console.log(gameScores)
+// each player
+    for(player of players)
+    {
 
+        let names = ''
 
+        // player name
+        names+= player[0] + ', '
+
+        // each game of that player
+            for (game of player[1])
+            {
+
+                // game name
+                names += game[0] + ', '
+
+            }
+
+        console.log(names.substring(0,names.length-2))
+
+    }
+
+console.log('\nScore Summary:')
+
+// goes through the scores
+    for(let i = 0; i < gameScores.length;i++)
+    {
+
+        let score = ''
+
+        // first item of each row is the players name
+            score += gameScores[i][0] + ' - Scores: '
+        
+        let tempList = []
+
+        // skips the first and last of each row
+            for(let j = 1; j < gameScores[i].length-1;j++)
+            {
+
+                tempList.push(gameScores[i][j])
+
+            }
+
+        // the last item of each row is the average score
+            score += tempList + ' | Average: ' + gameScores[i][gameScores[i].length-1]
+
+        console.log(score)
+
+    }
